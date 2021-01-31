5d78fad45cde8d2ca76bedd8d5f0798538a7ad83
SiddyJain70 <77151227+SiddyJain70@users.noreply.github.com>
2021-01-31 23:22:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("bt" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+// create questions
+var questions = [
+    new Question("I waited _____ the train arrived", ["till", "untill","where", "before"], "till"),
+
+    new Question("You will not succeed _____ you work harder", ["if", "unless","once", "wherever"], "unless"),
+    
+
+    new Question("You will get the prize _____ you deserve it", ["unless", "if","because", "once"], "if"),
+    
+
+    new Question("Do not go _____ I come", ["till", "before","unless", "untill"], "before"),
+        
+
+];
+
+
+var quiz = new Quiz(questions);
+
+
+populate();
